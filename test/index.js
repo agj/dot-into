9bef6d023b636f8cf7490498cdb418fc853bd750
agj <ale@agj.cl>
2015-11-18 01:48:26 +0900

    Added test for presence of 'into' function in exported module.

--- test/index.js
+++ test/index.js
@@ -12,10 +12,15 @@ function increment(a) { return new Num(a + 1) };
 function add(a, b, c, d) { return new Num(a + b + c + d) };
 
 test('Installation.', function (assert) {
+	assert.plan(1);
 	assert.doesNotThrow(function () {
 		into.install(Num.prototype);
 	});
-	assert.end();
+});
+
+test('Function exposed stand-alone.', function (assert) {
+	assert.plan(1);
+	assert.equal(into.into, Num.prototype.into);
 });
 
 test('Installed method is non-enumerable.', function (assert) {
@@ -26,11 +31,11 @@ test('Installed method is non-enumerable.', function (assert) {
 });
 
 test('Use without parameters.', function (assert) {
+	assert.plan(1);
 	assert.equal(new Num(1).into(increment).valueOf(), 2);
-	assert.end();
 });
 
 test('Use with parameters.', function (assert) {
+	assert.plan(1);
 	assert.equal(new Num(1).into(add, 1, 2, 3).valueOf(), 7);
-	assert.end();
 });
