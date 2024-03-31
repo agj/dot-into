2482d3a2acaa1befe24bf4d3e72cf822b69b7c47
agj <ale@agj.cl>
2024-03-31 18:49:35 -0300

    Improve tests

--- test/dotinto.test.ts
+++ test/dotinto.test.ts
@@ -1,7 +1,7 @@
 import "../src/dotinto";
 import { test, expect } from "bun:test";
 
-test("Installed method is non-enumerable.", function () {
+test("`into` method is non-enumerable in `Object` prototype.", function () {
   const findIntoProp = () => {
     for (var prop in Object.prototype) {
       if (prop === "into") {
@@ -16,11 +16,15 @@ test("Installed method is non-enumerable.", function () {
 test("Use without parameters.", function () {
   const input = [1, 2, 3] as const;
 
-  expect(input.into((arr) => arr[1])).toBe(2);
+  const result = input.into((arr) => arr[1]);
+
+  expect(result).toBe(2);
 });
 
 test("Use with parameters.", function () {
   const input = [1, 2, 3] as const;
 
-  expect(input.into((arr, num: number) => arr[1] + num, 5)).toBe(7);
+  const result = input.into((arr, num) => arr[1] + num, 5);
+
+  expect(result).toBe(7);
 });
