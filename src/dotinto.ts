287faed328e3032f92e265460bdd069aa7b29504
agj <ale@agj.cl>
2024-03-31 19:59:17 -0300

    Simplify by removing declarations file

--- src/dotinto.ts
+++ src/dotinto.ts
@@ -9,6 +9,22 @@ function into(
 
 // Install globally.
 
+declare global {
+  interface Object {
+    /**
+     * Allows you to pipe a value into a function in left-to-right order, like
+     * Bash `|` or some other languages' `|>` operator.
+     *
+     * @example
+     *   // Turn this:
+     *   third(second(first(a)), b);
+     *   // Into this:
+     *   first(a).into(second).into(third, b);
+     */
+    into: Dotinto;
+  }
+}
+
 Object.defineProperty(Object.prototype, "into", {
   value: into,
   enumerable: false,
